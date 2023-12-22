0d848df476f53beba2f9b7bb62c636325c509481
Alba Giraldo <agiraldomolina@gmail.com>
2023-12-22 12:39:34 -0500

    complete show user listings functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -26,7 +26,7 @@ import { hash } from "bcryptjs";
 // request.resource.contentType.matches('image/.*')
 
 export default function Profile() {
-  //const dispatch = useDispatch()
+  // Defining pieses of state
   const fileRef = useRef(null);
   const [showPassword, setShowPassword] = useState(false);
   const PasswordIcon = showPassword? FaEye : FaEyeSlash;
@@ -37,6 +37,8 @@ export default function Profile() {
   const [formData, setFormData] = useState({})
   const dispatch = useDispatch();
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [ showListingsError, setShowListingsError] = useState(false);
+  const [ userListings, setUserListing] = useState([]);
   //console.log(formData);
   // console.log(filePerc);
   // console.log(fileUploadError);
@@ -129,13 +131,43 @@ export default function Profile() {
     }
   }
 
+  const handleShowListings = async() => {
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingsError(true);
+        return;
+      }
+      setUserListing(data)
+    } catch (error) {
+      setShowListingsError(true);
+    }
+  }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className='text-3xl font-semibold text-center my-7'>profile</h1>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4" >
-        <input onChange={(e)=>setFile(e.target.files[0])} type="file"  ref={fileRef} hidden accept="image/.*"/>
-        <img onClick={()=>fileRef.current.click()} src={formData.avatar || currentUser.avatar} alt="profile" className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2" />
-        <p className="text-sm self-center">
+      <form
+        onSubmit={handleSubmit} 
+        className="flex flex-col gap-4" 
+       >
+        <input 
+          onChange={(e)=>setFile(e.target.files[0])} 
+          type="file"  
+          ref={fileRef} 
+          hidden accept="image/.*"
+        />
+        <img 
+          onClick={()=>fileRef.current.click()} 
+          src={formData.avatar || currentUser.avatar} 
+          alt="profile" 
+          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2" 
+        />
+        <p 
+          className="text-sm self-center"
+        >
           {fileUploadError ?
           (<span className="text-red-700">
             Error Image upload (image must be less than 2MB)
@@ -154,15 +186,21 @@ export default function Profile() {
               )        
           }
         </p>
-        <input type="text"
-         placeholder="username"
-         defaultValue={currentUser.username}
+        <input 
+          type="text"
+          placeholder="username"
+          defaultValue={currentUser.username}
           id="username"
-          className="border p-3 rounded-lg" onChange={handleChange}/> 
-        <input type="text"
-         placeholder="email"
-         defaultValue={currentUser.email} 
-          id="email" className="border p-3 rounded-lg" onChange={handleChange}/>
+          className="border p-3 rounded-lg" onChange={handleChange}
+          /> 
+        <input 
+          type="text"
+          placeholder="email"
+          defaultValue={currentUser.email} 
+          id="email" 
+          className="border p-3 rounded-lg" 
+          onChange={handleChange}
+        />
         <div className='bg-white p-3 border rounded-lg flex justify-between'>
         <input
           type={showPassword? 'text' : 'password'}
@@ -170,21 +208,92 @@ export default function Profile() {
           className='flex-grow bg-transparent focus:outline-none w-24 sm:w-64'
           id='password'
         />
-        <button type="button" onClick={() => setShowPassword(!showPassword)} className='focus:outline-none text-gray-500'>
+        <button 
+          type="button" 
+          onClick={() => setShowPassword(!showPassword)} 
+          className='focus:outline-none text-gray-500'
+        >
           <PasswordIcon className='text-slate-600'/>
         </button>
       </div>
-        <button disabled={loading} className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">{loading ? 'loading...' : "Update"}</button>
+        <button 
+          disabled={loading} 
+          className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">{loading ? 'loading...' : "Update"}
+        </button>
         <Link className="bg-green-700  text-white text-center rounded-lg p-3 uppercase hover:opacity-95" to={'/create-listing'}>
           Create Listing
         </Link>
       </form>
       <div className="flex justify-between mt-5">
-        <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete account</span>
-        <span onClick={handleSignOutUser} className="text-red-700 cursor-pointer">Sign Out</span>
+        <span 
+          onClick={handleDeleteUser} 
+          className="text-red-700 cursor-pointer"
+        >
+          Delete account
+        </span>
+        <span 
+          onClick={handleSignOutUser} 
+          className="text-red-700 cursor-pointer"
+        >
+          Sign Out
+        </span>
       </div>
-      <p className="text-red-700 mt-5 text-center " >{error? error :''}</p>
-      <p className="text-green-700 mt-5 text-center " >{updateSuccess? "User is updated successfully!" :''}</p>
+      <p 
+        className="text-red-700 mt-5 text-center " 
+      >
+        {error? error :''}
+      </p>
+      <p 
+        className="text-green-700 mt-5 text-center " 
+      >
+        {updateSuccess? "User is updated successfully!" :''}
+      </p>
+      <button
+        className="text-green-700 text-center w-full rounded-lg p-3 hover:opacity-95"
+        onClick={handleShowListings}
+      >
+        Show Listings
+      </button>
+      <p 
+        className="text-red-700 mt-5 text-center " 
+      >
+        {showListingsError? 'Error showing listings' :''}
+      </p>
+      
+      {userListings &&  userListings.length > 0 &&
+      <div className="flex flex-col gap-4">
+        <h1 className="text-center mt-7 text-2xl font-semibold">Your Listings</h1>
+
+        {userListings.map((listings, index)=> (
+          <div
+            key={index}
+            className="border p-3 rounded-lg flex  justify-between items-center gap-4"
+          >
+            <Link to={`/listings/${listings._id}`} className="text-blue-700">
+              <img 
+                src={listings.imageUrls[0]} 
+                alt="listing cover" 
+                className="h-16 w-16 object-contain rounded-lg"
+              />
+            </Link>
+            <Link
+              className="flex-1 text'slate-700 font-semibold hover:underline truncate"
+              to={`/listings/${listings._id}`}
+            >
+              <p>{listings.name}</p>
+            </Link>
+            <div className="flex flex-col items-center">
+              <button className="text-red-700 uppercase">Delete</button>
+              <button className="text-green-700 uppercase">Edit</button>
+
+            </div>
+
+          </div>
+
+        ))
+      
+       }
+      </div>}
     </div>
   )
 }

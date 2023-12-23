ab5324e9bd765f991192db4ba9a4885c0f92c301
Alba Giraldo <agiraldomolina@gmail.com>
2023-12-23 01:25:38 -0500

    complete listing page

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -269,7 +269,9 @@ export default function createListing() {
                         />
                         <div className='flex flex-col items-center'>
                             <p>Regular price</p>
-                            <span className='text-xs'>($/month)</span>
+                            {formData.type === 'rent' && (
+                                <span className='text-xs'>($/month)</span>
+                            )}
                         </div>
                     </div>
                     {formData.offer && (
@@ -286,7 +288,9 @@ export default function createListing() {
                         />
                         <div className='flex flex-col items-center'>
                             <p>Discount price</p>
-                            <span className='text-xs'>($/month)</span>
+                            {formData.type === 'rent' && (
+                                <span className='text-xs'>($/month)</span>
+                            )}
                         </div>
                     </div>
                     )}
